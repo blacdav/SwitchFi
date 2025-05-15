956b367067f970eddf688e34d632c1694dbf7342
Blacdav <davidaniefoik@gmail.com>
2025-05-15 15:48:10 +0100

    new update

--- src/app/get_started/page.tsx
+++ src/app/get_started/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <main className='grid grid-rows-12 h-screen p-5'>
+    <main className='grid grid-rows-12 h-dvh p-5'>
       <Image src='/assets/Exchange.png' alt='logo' width={300} height={300} className='row-span-9 m-auto' />
       <p className='px-3 text-base font-semibold text-center'><span className='text-primary'>SwitchFi</span> nulla suspendisse da Lorem ipsum dolor sit amet <span className='text-primary'>consectetur.</span>Varius orci</p>
       <Link href='/onboarding' className='font-semibold text-sm mt-3 bg-gradient-to-b from-secondary to-primary rounded-md text-white h-14 grid justify-center items-center'>Lets Get Started</Link>
