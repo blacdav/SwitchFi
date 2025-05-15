0e6ae2091ea9910fa85bbca1c32d8f540954635e
Blacdav <davidaniefoik@gmail.com>
2025-05-15 19:22:46 +0100

    new update on the get started screen

--- src/app/get_started/page.tsx
+++ src/app/get_started/page.tsx
@@ -6,10 +6,10 @@ import React from 'react'
 const Page = () => {
   return (
     <main className='grid grid-rows-12 h-dvh p-5'>
-      <Image src='/assets/Exchange.png' alt='logo' width={300} height={300} className='row-span-9 m-auto' />
+      <Image src='/assets/Exchange.png' alt='logo' width={800} height={800} className='w-80 h-80 row-span-9 m-auto' />
       <p className='px-3 text-base font-semibold text-center'><span className='text-primary'>SwitchFi</span> nulla suspendisse da Lorem ipsum dolor sit amet <span className='text-primary'>consectetur.</span>Varius orci</p>
       <Link href='/onboarding' className='font-semibold text-sm mt-3 bg-gradient-to-b from-secondary to-primary rounded-md text-white h-14 grid justify-center items-center'>Lets Get Started</Link>
-      <p className='font-semibold text-sm mt-3 mx-auto'>Already have an account? <span className='text-primary underline'><Link href=''>Log In</Link></span></p>
+      <p className='font-semibold text-sm mt-5 mx-auto'>Already have an account? <span className='text-primary underline'><Link href=''>Log In</Link></span></p>
     </main>
   )
 }
